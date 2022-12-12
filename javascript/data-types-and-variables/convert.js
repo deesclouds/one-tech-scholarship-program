ff302fec0cc3e9f20d843c23fbd5a95fcaa4a6ee
deesclouds <deescloud@protonmail.com>
2022-12-12 14:56:10 -0800

    add files to folder

---
+++ javascript/data-types-and-variables/convert.js
@@ -0,0 +1,20 @@
+// To convert Celsius to Fahrenheit use this formula:
+// F = C * 1.8 + 32
+
+/* Directions: Use this equation and the variables fahrenheit and celsius to print the Fahrenheit equivalent of 12˚C.
+
+Note: "12˚C" reads as "12 degrees Celsius".
+
+Running code: Enter node convert.js in the terminal
+
+
+1. Set the fahrenheit variable to the correct value using the celsius variable and the formula above.
+2. Log the fahrenheit variable to the console.
+
+*/
+
+const celsius = 12;
+const fahrenheit = celsius * 1.8 + 32;
+
+console.log(fahrenheit)
+
---
+++ javascript/data-types-and-variables/equality.js
@@ -0,0 +1,14 @@
+/* Directions: Fix the right expression so it evaluates to true.
+
+"ALL Strings are CrEaTeD equal" == "All STRINGS are CrEaTED Equal"
+
+
+Running code:
+
+Enter node equality.js into the terminal.
+
+*/
+
+const answer = "ALL Strings are CrEaTeD equal" == "ALL Strings are CrEaTeD equal"
+
+console.log(answer);
