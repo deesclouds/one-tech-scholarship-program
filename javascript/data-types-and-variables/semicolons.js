e2931505c27784542e3a7323f32c796eb9be5585
deesclouds <deescloud@protonmail.com>
2022-12-14 23:33:32 -0800

    use concatenation for two strings

---
+++ javascript/data-types-and-variables/semicolons.js
@@ -0,0 +1,12 @@
+// Semicolons
+
+// Directions: define two variables called thingOne and thingTwo and assign them values. Print both values. Print the values of both in one console.log using concatenation. 
+
+// Ex: "red" is the value of thingOne and "blue" is the value of thingTwo. Don't forget to use semicolon at the end of each statement!
+
+// When running code enter node semicolons.js into the terminal.
+
+const thingOne = "Rosie";
+const thingTwo = "Molly";
+
+console.log(thingOne + " " + thingTwo);
