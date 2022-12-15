8825c96c67e018290d917f828b6ae10b118e29d4
deesclouds <deescloud@protonmail.com>
2022-12-14 23:37:17 -0800

    add name as string

---
+++ javascript/data-types-and-variables/name.js
@@ -0,0 +1,7 @@
+// Directions: Create a variable fullName and assign it your full name as a string. Print the value of the variable in the console.
+
+// Running code run node name.js in the terminal.
+
+const fullName = "Darnell";
+
+console.log(fullName)
