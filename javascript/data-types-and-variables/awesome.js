9eaf1430bf04749edf7c5779ca4a99d99a7c3fec
deesclouds <deescloud@protonmail.com>
2022-12-15 00:11:45 -0800

    add an awesome message

---
+++ javascript/data-types-and-variables/awesome.js
@@ -0,0 +1,9 @@
+
+let firstName = "Darnell";
+let interest = "to code";
+let hobby = "deep house music";
+
+let awesomeMessage = 
+"Hi, my name is " + firstName + ". I love " + interest +". In my spare time, I like to play " + hobby + ".";
+
+console.log(awesomeMessage);
