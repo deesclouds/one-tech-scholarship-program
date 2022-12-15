034e87f5efef44eeb87e3bb7a759302746d32752
deesclouds <deescloud@protonmail.com>
2022-12-15 00:02:08 -0800

    add three adjectives to story

---
+++ javascript/data-types-and-variables/mad-libs.js
@@ -0,0 +1,9 @@
+// Directions: Mad Libs is a word game where players have fun substituting words for blanks in a story. For this exercise, use the adjective variables below to fill in the blanks and complete the following message. 
+const adjective1 = "amazing";
+const adjective2 = "fun";
+const adjective3 = "entertaining";
+
+const madLib = "The Intro to JavaScript course is " + adjective1 + ". James and Julia are so " + adjective2 + ". I cannot wait to work through the rest of this " + adjective3 + " content!";
+
+
+console.log(madLib);
