6147e3f5ef28524a4a1a41d43fef36f6dfacdee8
deesclouds <deescloud@protonmail.com>
2022-12-26 14:41:27 -0800

    practicing with truthy and falsey values

---
+++ javascript/advanced-conditionals/truthy-or-falsey.js
@@ -0,0 +1,15 @@
+if (1 == true)
+console.log("Hi")
+
+if(1 != true)
+console.log("Run")
+
+if(0 == false && 1 != true)
+console.log('Cat')
+
+if(false || 0 || "" == false)
+console.log('Dog')
+
+if(0 == false || 1 != true)
+console.log('Frog')
+
