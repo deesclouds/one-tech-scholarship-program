a71eef490a340a57d3fb0b3f81abb60759d2bf96
deesclouds <deescloud@protonmail.com>
2022-12-28 13:17:00 -0800

    fix errors in for loopand code is running without errors

---
+++ javascript/loops/fix2.js
@@ -0,0 +1,28 @@
+/*
+Directions: 
+The for loop below has an error. Fix it!
+
+for (let k = 12 k < 21 k++){
+    console.log(k);
+}
+
+Running code: 
+node fix2.js
+*/
+for (let k = 12; k <= 20; k++){
+    console.log(k);
+}
+/*
+Testing code:
+Make sure to use a for loop and code runs without errors:
+
+12
+13
+14
+15
+16
+17
+18
+19
+20
+*/
