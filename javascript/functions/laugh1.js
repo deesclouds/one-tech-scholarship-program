4c8f6b9c3814027107d8ceb949e77fcb481d619a
deesclouds <deescloud@protonmail.com>
2022-12-28 14:32:15 -0800

    function returns a string

---
+++ javascript/functions/laugh1.js
@@ -0,0 +1,14 @@
+/*
+Directions:
+Declare a function called laugh() that returns "hahahahahahahahahaha!"
+Print the value returned from the laugh() function to the console.
+
+Running code: 
+node laugh1.js
+*/
+
+function laugh(){
+    const message = "hahahahahahahahahaha!";
+    return message;
+}
+console.log(laugh());
