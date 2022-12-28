2b71e55bb1ce2e235e1bde839dbefe9bf05a2ce6
deesclouds <deescloud@protonmail.com>
2022-12-28 13:13:55 -0800

    fix erros in the for loop to get it running smoothly without errors

---
+++ javascript/loops/fix1.js
@@ -0,0 +1,25 @@
+/*
+Directions: 
+Here is a for loop that's supposed to print the numbers 5 - 9. Fix the errors!
+
+for (x < 10; x++){
+    console.log(x);
+}
+
+Running code:
+node fix1.js
+*/
+
+for (let x = 5; x < 10; x++){
+    console.log(x);
+}
+
+/*
+Testing code:
+Make sure for loop runs without errors.
+5
+6
+7
+8
+9
+*/
